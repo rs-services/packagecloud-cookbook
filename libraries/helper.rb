5ba0827b4047ad24b46b9fee07759947141ef848
Richard Shade <rshade@rightscale.com>
2015-03-10 12:50:04 -0500

    fixing file log

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -2,13 +2,18 @@ require 'net/https'
 
 module PackageCloud
   module Helper
+    def file_log(message)
+      f=File.open('/tmp/packagecloud.log','a+')
+      f.write "#{message}\n"
+      f.close
+    end
     def get(uri, params)
-      f=File.open('/tmp/packagecloud.log','w+')
+      file_log "begin"
       uri.query     = URI.encode_www_form(params)
       req           = Net::HTTP::Get.new(uri.request_uri)
 
       req.basic_auth uri.user, uri.password if uri.user
-      f.write "Uri:#{uri.hostname}:#{uri.port}"
+      file_log "Uri:#{uri.hostname}:#{uri.port}"
       http = Net::HTTP.new(uri.hostname, uri.port)
       if uri.port == 443
         http.use_ssl = false
@@ -24,9 +29,8 @@ module PackageCloud
       else
         http.use_ssl = false
       end
-      f.write "starting response"
+      file_log "starting response"
       resp = http.start { |h| h.request(req) }
-      f.close
       case resp
       when Net::HTTPSuccess
         resp
@@ -51,7 +55,7 @@ module PackageCloud
         if File.exists?('/etc/pki/tls/certs/ca-bundle-new.crt')
           http.cert_store.add_file('/etc/pki/tls/certs/ca-bundle-new.crt')
         else
-          Chef::Log.info "new bundle does not exist"
+          file_log "new bundle does not exist"
         end
       else
         http.use_ssl = false
